10081378ab5bfa1eebb6c88f0d75993c5893d121
BeagleBoard <beagle@beagleboard.org>
2022-06-29 09:27:01 -0400

    Converted blinkLED.py to not use Adafruit_BBIO

--- BeagleBone/Black/blinkLED.py
+++ BeagleBone/Black/blinkLED.py
@@ -8,15 +8,33 @@
 #	Setup:	
 #	See:	
 #//////////////////////////////////////
-import Adafruit_BBIO.GPIO as GPIO
+# import Adafruit_BBIO.GPIO as GPIO
 import time
+import os
 
-out = "P9_14"
+# out = "P9_14"     Look up using show-pins
+gpio = "50"
  
-GPIO.setup(out, GPIO.OUT)
+# GPIO.setup(out, GPIO.OUT)
+PATH='/sys/class/gpio/'
+# export gpio pin if not already
+if (not os.path.exists(PATH+"gpio"+gpio)):
+    f = open(PATH+"export", "w")
+    f.write(gpio)
+    f.close()
+# Set direction to "out"
+f = open(PATH+"gpio"+gpio+"/direction", "w")
+f.write("out")
+f.close()
  
+f = open(PATH+"gpio"+gpio+"/value", "w")
 while True:
-    GPIO.output(out, GPIO.HIGH)
+    # GPIO.output(out, GPIO.HIGH)
+    f.seek(0)
+    f.write("1")
     time.sleep(0.5)
-    GPIO.output(out, GPIO.LOW)
+    # GPIO.output(out, GPIO.LOW)
+    f.seek(0)
+    f.write("0")
     time.sleep(0.5)
+f.close()
