9fb252f90561fc5b3051a634ed47ddb43a622bb7
Ayush Singh <ayush@beagleboard.org>
2025-02-22 14:44:14 +0530

    PocketBeagle2: blinky: Fix import
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- PocketBeagle2/blinky/rust/src/main.rs
+++ PocketBeagle2/blinky/rust/src/main.rs
@@ -2,7 +2,7 @@ use std::thread::sleep;
 use std::time::Duration;
 
 use beagle_linux_sdk::boards::pocketbeagle2::P1_20;
-use beagle_linux_sdk::led::Led;
+use beagle_linux_sdk::Led;
 
 fn main() {
     let led = Led::new(P1_20).expect("Failed to open LED PIN");
