ccb2182c9da9aef32b6d3c9fc9c5777c8c79dc12
Ayush Singh <ayush@beagleboard.org>
2025-03-07 19:47:06 +0530

    PocketBeagle-2: blinky: python: Use USR_LED4
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- Libraries/python/beagle_linux_sdk/src/beagle_linux_sdk/boards/pocketbeagle2.py
+++ Libraries/python/beagle_linux_sdk/src/beagle_linux_sdk/boards/pocketbeagle2.py
@@ -8,6 +8,11 @@ P1_36 = Pin(pwm=PwmPin(0, 0))
 P2_30 = Pin(pwm=PwmPin(2, 0))
 P2_33 = Pin(gpio=GpioPin("2", 52))
 
+USR_LED1 = Path("/sys/class/leds/beaglebone:green:usr1")
+USR_LED2 = Path("/sys/class/leds/beaglebone:green:usr2")
+USR_LED3 = Path("/sys/class/leds/beaglebone:green:usr3")
+USR_LED4 = Path("/sys/class/leds/beaglebone:green:usr4")
+
 EEPROM_PATH = "/sys/bus/i2c/devices/0-0050/eeprom"
 
 TECHLAB_SEVEN_SEGMENT_LEFT = Path("/sys/devices/platform/seven-segments-left/linedisp.1/")
--- Libraries/python/beagle_linux_sdk/src/beagle_linux_sdk/devices/led.py
+++ Libraries/python/beagle_linux_sdk/src/beagle_linux_sdk/devices/led.py
@@ -1,7 +1,46 @@
+from pathlib import Path
 from ..pin import Pin
 
 
 class Led:
+    def __init__(self, pin: Pin | Path) -> None:
+        if isinstance(pin, Pin):
+            self.inner = _GpioLed(pin)
+        elif isinstance(pin, Path):
+            self.inner = _SysfsLed(pin)
+        else:
+            raise ValueError("Invalid pin")
+
+    def on(self):
+        self.inner.on()
+
+    def off(self):
+        self.inner.off()
+
+
+class _SysfsLed:
+    def __init__(self, base_dir: Path) -> None:
+        if not base_dir.is_dir():
+            raise ValueError("RGB LED does not exist")
+
+        self.max_brightness = self.__read_max_brightness(base_dir)
+        self.brightness = base_dir.joinpath("brightness")
+
+    def on(self):
+        with open(self.brightness, "w") as f:
+            f.write(self.max_brightness)
+
+    def off(self):
+        with open(self.brightness, "w") as f:
+            f.write("0")
+
+    @staticmethod
+    def __read_max_brightness(base_dir: Path) -> str:
+        with open(base_dir.joinpath("max_brightness"), "r") as f:
+            return f.read()
+
+
+class _GpioLed:
     def __init__(self, pin: Pin) -> None:
         self.pin = pin.gpio_output()
 
