a4741eae0d73a373654d3f7c2c320dad33a12fb6
Ayush Singh <ayush@beagleboard.org>
2025-03-07 19:48:03 +0530

    Libraries: rust: beagle_linux_sdk: Rename leds
    - USER_LED -> USR_LED
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- PocketBeagle-2/blinky/rust/src/main.rs
+++ PocketBeagle-2/blinky/rust/src/main.rs
@@ -1,11 +1,11 @@
 use std::thread::sleep;
 use std::time::Duration;
 
-use beagle_linux_sdk::boards::pocketbeagle2::USER_LED4;
+use beagle_linux_sdk::boards::pocketbeagle2::USR_LED4;
 use beagle_linux_sdk::Led;
 
 fn main() {
-    let mut led = Led::new(USER_LED4).expect("Failed to open LED PIN");
+    let mut led = Led::new(USR_LED4).expect("Failed to open LED PIN");
 
     loop {
         println!("ON");
