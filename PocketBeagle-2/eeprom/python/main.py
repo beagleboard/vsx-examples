83c3b3d0f9fcb33dc5778d04dce0067db9fa369d
Ayush Singh <ayush@beagleboard.org>
2025-03-08 13:43:54 +0530

    CI: Fix CI
    - Also add building python examples
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- PocketBeagle-2/eeprom/python/main.py
+++ PocketBeagle-2/eeprom/python/main.py
@@ -1,5 +1,5 @@
 from beagle_helper import BeagleEeprom
-from beagle_helper.board.pocketbeagle2 import EEPROM_PATH
+from beagle_helper.boards.pocketbeagle2 import EEPROM_PATH
 from pprint import pprint
 
 eeprom_data = BeagleEeprom.read(EEPROM_PATH)
