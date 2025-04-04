224c00bea2830ef79421a1037f61fd2af9d3e258
Ayush Singh <ayush@beagleboard.org>
2025-04-04 23:42:10 +0530

    PocketBeagle-2: accelerometer: rust: Use sysfs entries
    - Reduce the level of abstraction.
    - Just use very thin wrappers around reading sysfs entries
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- Libraries/rust/beagle_helper/src/lib.rs
+++ Libraries/rust/beagle_helper/src/lib.rs
@@ -2,6 +2,8 @@ pub(crate) mod abstractions;
 pub mod boards;
 pub mod devices;
 pub(crate) mod pin;
+#[cfg(feature = "sysfs")]
+pub mod sysfs;
 
 #[allow(unused_imports)]
 pub use devices::*;
--- PocketBeagle-2/accelerometer/rust/src/main.rs
+++ PocketBeagle-2/accelerometer/rust/src/main.rs
@@ -1,16 +1,23 @@
 use std::{thread::sleep, time::Duration};
 
-use beagle_helper::{boards::pocketbeagle2::techlab, Accel};
+use beagle_helper::sysfs;
+
+const DEV_NAME: &str = "mma8453";
 
 fn main() {
-    let accl = Accel::new(techlab::ACCELEROMETER).unwrap();
+    let accl = sysfs::Device::with_dev_name(DEV_NAME).unwrap();
+
+    let scale = accl.sysfs_r("in_accel_scale").unwrap().read_f64().unwrap();
+    let mut x_raw = accl.sysfs_r("in_accel_x_raw").unwrap();
+    let mut y_raw = accl.sysfs_r("in_accel_y_raw").unwrap();
+    let mut z_raw = accl.sysfs_r("in_accel_z_raw").unwrap();
 
     loop {
         println!(
-            "X = {}, Y = {}, Z = {}",
-            accl.x_raw().unwrap(),
-            accl.y_raw().unwrap(),
-            accl.z_raw().unwrap()
+            "Acceleration along X = {:.2} ms^2, Y = {:.2} ms^2, Z = {:.2} ms^2",
+            x_raw.read_f64().unwrap() * scale,
+            y_raw.read_f64().unwrap() * scale,
+            z_raw.read_f64().unwrap() * scale,
         );
 
         sleep(Duration::from_secs(1));
