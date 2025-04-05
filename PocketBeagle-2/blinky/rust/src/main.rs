bda9d792bd7934d626d813ef955b1254cf0d9640
Ayush Singh <ayush@beagleboard.org>
2025-04-05 17:18:25 +0530

    PocketBeagle-2: blinky: rust: Update to use raw sysfs
    - Using sysfs directly for LED
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- PocketBeagle-2/blinky/rust/src/main.rs
+++ PocketBeagle-2/blinky/rust/src/main.rs
@@ -1,19 +1,30 @@
 use std::thread::sleep;
 use std::time::Duration;
 
-use beagle_helper::boards::pocketbeagle2::USR_LED4;
-use beagle_helper::Led;
+use beagle_helper::sysfs;
+
+const LED: &str = "/sys/class/leds/beaglebone:green:usr4";
 
 fn main() {
-    let mut led = Led::new(USR_LED4).expect("Failed to open LED PIN");
+    let led = sysfs::Device::with_path(LED).unwrap();
+
+    // Technically, max_brightness will be an unsigned integer value. However, since we never
+    // actually parse it, and writing to the file needs conversion back to string anyway, it is
+    // better to just keep it as string
+    let max_brightness = led
+        .sysfs_r("max_brightness")
+        .unwrap()
+        .read_string()
+        .unwrap();
+    let mut brightness = led.sysfs_w("brightness").unwrap();
 
     loop {
         println!("ON");
-        led.on().unwrap();
+        brightness.write_string(&max_brightness).unwrap();
         sleep(Duration::from_secs(1));
 
         println!("OFF");
-        led.off().unwrap();
+        brightness.write_string("0").unwrap();
         sleep(Duration::from_secs(1));
     }
 }
