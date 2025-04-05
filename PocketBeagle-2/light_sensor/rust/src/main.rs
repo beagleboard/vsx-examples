70c5fa9aa8a74c7e93274257c977af890f26895e
Ayush Singh <ayush@beagleboard.org>
2025-04-05 14:43:35 +0530

    PocketBeagle-2: light_sensor: rust: Use sysfs
    - Switch to raw sysfs abstractions
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- PocketBeagle-2/light_sensor/rust/src/main.rs
+++ PocketBeagle-2/light_sensor/rust/src/main.rs
@@ -1,14 +1,23 @@
 use std::thread::sleep;
 use std::time::Duration;
 
-use beagle_helper::boards::pocketbeagle2::P1_19;
-use beagle_helper::LightSensor;
+use beagle_helper::sysfs;
+
+/// Reading values from ADC directly
+const DEV_NAME: &str = "ad7291";
+
+const THRESHOLD: f64 = 2000.0;
 
 fn main() {
-    let mut ldr = LightSensor::new(P1_19, None).unwrap();
+    let adc = sysfs::Device::with_dev_name(DEV_NAME).unwrap();
+
+    let mut ldr_raw = adc.sysfs_r("in_voltage0_raw").unwrap();
+    let scale = adc.sysfs_r("in_voltage_scale").unwrap().read_f64().unwrap();
 
     loop {
-        if ldr.value().unwrap() {
+        let scaled = ldr_raw.read_f64().unwrap() * scale;
+
+        if scaled > THRESHOLD {
             println!("Light");
         } else {
             println!("Dark");
