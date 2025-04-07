8c9f79f52a171020f9d68f585f0e68aa144448e2
Ayush Singh <ayush@beagleboard.org>
2025-04-07 17:42:41 +0530

    Libraries: python: beagle_helper: Remove led abstraction
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- Libraries/python/beagle_helper/src/beagle_helper/__init__.py
+++ Libraries/python/beagle_helper/src/beagle_helper/__init__.py
@@ -1,4 +1,3 @@
-from .devices.led import Led
 from .devices.button import Button
 from .devices.pwm_led import PwmLed
 from .devices.beagle_eeprom import BeagleEeprom
--- Libraries/python/beagle_helper/src/beagle_helper/pin.py
+++ Libraries/python/beagle_helper/src/beagle_helper/pin.py
@@ -2,20 +2,12 @@ import gpiod
 
 from .abstractions.pwm import PwmChip, PwmChannel
 from time import sleep
-from dataclasses import dataclass
 
 
 class GpioPin:
     def __init__(self, chipset: str, offset: int) -> None:
         self.chipset = chipset
         self.offset = offset
-
-    def gpio_output(self):
-        chip = gpiod.Chip(self.chipset)
-        led = chip.get_line(self.offset)
-        led.request(consumer="", type=gpiod.LINE_REQ_DIR_OUT)
-
-        return led
 
     def gpio_input(self):
         chip = gpiod.Chip(self.chipset)
@@ -39,12 +31,6 @@ class Pin:
     def __init__(self, gpio: GpioPin | None = None, pwm: PwmPin | None = None) -> None:
         self.gpio = gpio
         self.pwm = pwm
-
-    def gpio_output(self):
-        if self.gpio == None:
-            raise ValueError("Pin cannot be used as GPIO")
-
-        return self.gpio.gpio_output()
 
     def gpio_input(self):
         if self.gpio == None:
