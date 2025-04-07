b45783178538292a1cdcd372b9632e80e21a50df
Ayush Singh <ayush@beagleboard.org>
2025-04-07 19:46:07 +0530

    Libraries: python: beagle_helper: Remove seven_segment abstractions
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- Libraries/python/beagle_helper/src/beagle_helper/__init__.py
+++ Libraries/python/beagle_helper/src/beagle_helper/__init__.py
@@ -1,6 +1,5 @@
 from .devices.button import Button
 from .devices.pwm_led import PwmLed
 from .devices.beagle_eeprom import BeagleEeprom
-from .devices.seven_segment import SevenSegment
 from .devices.tonal_buzzer import TonalBuzzer
 from .devices.rgb_led import RgbLed
