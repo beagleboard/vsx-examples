acb7239da7dee7aeea767896927353547422cf5c
Jason Kridner <jkridner@beagleboard.org>
2025-04-08 15:09:25 -0400

    Create PWM pseudocode

--- PocketBeagle-2/tonal_buzzer/rust/src/main.rs
+++ PocketBeagle-2/tonal_buzzer/rust/src/main.rs
@@ -5,11 +5,20 @@
 use std::thread::sleep;
 use std::time::Duration;
 
-use beagle_helper::boards::pocketbeagle2::P2_30;
-use beagle_helper::tonal_buzzer::{Tone, TonalBuzzer};
+use beagle_helper::sysfs;
+use beagle_helper::tonal_buzzer::Tone;
 
-// Hedwig's theme fromn the Harry Potter Movies
+// PWM_3 refers to the PWM provided by PRU0 PWM firmware.
+const PWMCHIP_EXPORT_PATH: &str = "/dev/pocket/pwm/3/export";
+const PWMCHIP_UNEXPORT_PATH: &str = "/dev/pocket/pwm/3/unexport";
+
+// PWM_3 channel 3 is exported on header pin P2_30.
+const PWM_CHANNEL: &str = "3";
+const PWM_PATH: &str = "/dev/pocket/pwm/3/pwm3";
+
+// Hedwig's theme from the Harry Potter Movies
 // Score from https://musescore.com/user/3811306/scores/4906610
+// NOTE: From a readability standpoint, this could be moved out of main.rs.
 const MELODY: &[(Option<Tone>, i32)] = &[
     // Opening
     (None, 2),
@@ -83,22 +92,27 @@ const TEMPO: f64 = 144.0;
 const WHOLE_NOTE: f64 = (60000.0 * 4.0) / TEMPO;
 
 fn main() {
-    let buzzer = TonalBuzzer::new(P2_30).unwrap();
+    let buzzer = sysfs::Device::with_export(PWMCHIP_EXPORT_PATH, PWM_CHANNEL, PWM_PATH).unwrap();
+    let period = buzzer.sysfs_w("period").unwrap();
+    let enable = buzzer.sysfs_w("enable").unwrap();
+    let duty_cycle = buzzer.sysfs_w("duty_cycle").unwrap();
 
     println!("Start Playing");
 
-    for (t, d) in MELODY {
-        let node_dur = if *d > 0 {
-            WHOLE_NOTE / d.abs() as f64
+    for (tone, dur) in MELODY {
+        let node_dur = if *dur > 0 {
+            WHOLE_NOTE / dur.abs() as f64
         } else {
-            (WHOLE_NOTE / d.abs() as f64) * 1.5
+            (WHOLE_NOTE / dur.abs() as f64) * 1.5
         };
 
-        buzzer.play(*t).unwrap();
+        period.write_string(tone.period_ns()).unwrap();
+        duty_cycle.write_string(tone.duty_cycle_ns()).unwrap();
+        enable.write(b"1").unwrap();
         sleep(Duration::from_millis((node_dur * 0.9) as u64));
-        buzzer.stop().unwrap();
+        enable.write(b"0").unwrap();
         sleep(Duration::from_millis((node_dur * 0.1) as u64));
     }
 
-    buzzer.stop().unwrap();
+    pwm = Sysfs::unexport(PWMCHIP_UNEXPORT_PATH, PWM_CHANNEL).unwrap();
 }
