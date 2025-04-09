8fcf982a4fedd915876b35cd322a0174d962ad84
Ayush Singh <ayush@beagleboard.org>
2025-04-09 18:10:37 +0530

    Libraries: rust: beagle_helper: Remove rgb_led abstraction
    - The example has been migrated to use sysfs.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- Libraries/rust/beagle_helper/src/devices/mod.rs
+++ Libraries/rust/beagle_helper/src/devices/mod.rs
@@ -2,8 +2,6 @@
 pub mod beagle_eeprom;
 #[cfg(feature = "button")]
 pub mod button;
-#[cfg(feature = "rgb_led")]
-pub mod rgb_led;
 #[cfg(feature = "tonal_buzzer")]
 pub mod tonal_buzzer;
 
@@ -11,7 +9,5 @@ pub mod tonal_buzzer;
 pub use beagle_eeprom::*;
 #[cfg(feature = "button")]
 pub use button::Button;
-#[cfg(feature = "rgb_led")]
-pub use rgb_led::RgbLed;
 #[cfg(feature = "tonal_buzzer")]
 pub use tonal_buzzer::TonalBuzzer;
