184b9887a5b506108b944113e6272fc873c2b2af
Jason Kridner <jkridner@beagleboard.org>
2025-04-09 09:12:22 -0400

    PRU: WIP

---
+++ Libraries/pru/pwm.am62_pru0.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */ 
+/* Copyright (C) 2025, Jason Kridner, BeagleBoard.org Foundation <jkridner@beagleboard.org> */
+
+/*
+ * pwm.am62_pru0.c - Example PWM firmware for AM62
+ */
+
+ int main() {
+        return(0);
+ }
