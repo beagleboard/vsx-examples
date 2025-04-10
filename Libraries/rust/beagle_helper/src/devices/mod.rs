c7b36bc85efdf51faf9f6aab7e156f7276a77d0d
Ayush Singh <ayush@beagleboard.org>
2025-04-10 19:56:54 +0530

    Libraries: rust: beagle_helper: Remove eeprom abstraction
    - Moved into the example itself.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- Libraries/rust/beagle_helper/src/devices/mod.rs
+++ Libraries/rust/beagle_helper/src/devices/mod.rs
@@ -1,9 +1,5 @@
-#[cfg(feature = "beagle_eeprom")]
-pub mod beagle_eeprom;
 #[cfg(feature = "tonal_buzzer")]
 pub mod tonal_buzzer;
 
-#[cfg(feature = "beagle_eeprom")]
-pub use beagle_eeprom::*;
 #[cfg(feature = "tonal_buzzer")]
 pub use tonal_buzzer::TonalBuzzer;
