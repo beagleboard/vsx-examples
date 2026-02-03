9a7f8a8bc608e67c76242e51d3d643291fc2d9b8
Ayush <ayushsingh1325@gmail.com>
2026-02-03 14:08:49 +0530

    Revert "Revert "PocketBeagle-2: rgb_led: LED Path has changed after update""

--- PocketBeagle-2/rgb_led/hue/python/main.py
+++ PocketBeagle-2/rgb_led/hue/python/main.py
@@ -18,7 +18,7 @@ def wheel(pos: int) -> tuple[int, int, int]:
         return (pos * 3, 255 - pos * 3, 0)
 
 
-LED = Path("/sys/devices/platform/techlab-led/leds/multi-led/")
+LED = Path("/sys/devices/platform/techlab-led/leds/rgb:/")
 DELAY = 0.01
 
 led = Device(path=LED)
